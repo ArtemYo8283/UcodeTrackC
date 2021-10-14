be351252ae66c8cdf6f10aa2dd9286839454f124
ArtemYo8283 <63734152+ArtemYo8283@users.noreply.github.com>
2021-10-14 23:32:27 +0300

    v0.5

---
+++ Ucode LibMx/src/main.c
@@ -0,0 +1,8 @@
+#include "libmx.h"
+
+int main()
+{
+    printf("%s",mx_itoa(85));
+    return 0;
+}
+
---
+++ Ucode LibMx/src/mx_strstr.c
@@ -0,0 +1,29 @@
+#include "libmx.h"
+
+char *mx_strstr(const char *haystack, const char *needle)
+{
+    char *h = (char *)haystack;
+    char *n = (char *)needle;
+    if (!h)
+    {
+        return NULL;
+    }
+    else if (!n)
+    {
+        return NULL;
+    }
+    else if (!mx_strlen(n))
+    {
+        return h;
+    }
+    while (*h) 
+    {
+        if (!mx_strncmp(h, n, mx_strlen(n)))
+        {
+            return h;
+        }
+        h++;
+    }
+    return NULL;
+}
+
