9cf442897e63a380a5f9ab260780052dfad209a2
ArtemYo8283 <63734152+ArtemYo8283@users.noreply.github.com>
2021-11-11 11:03:07 +0200

    Uls

--- Ucode uls/inc/uls.h
+++ Ucode uls/inc/uls.h
@@ -1,3 +1,14 @@
 #pragma once
 #include "libmx.h"
+#include <errno.h>
+#include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <sys/acl.h>
+#include <sys/ioctl.h>
+#include <time.h>
+
 
